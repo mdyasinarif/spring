afa4083004f6498f40296aca8be18124c659b17b
mdyasinarif <mdyasinarifidb49@gmail.com>
2019-07-10 19:13:01 +0600

    cms

---
+++ cms/src/main/java/com/cms/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.cms.controller;
+
+
+import com.cms.entity.User;
+import com.cms.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@Controller
+public class TestController {
+    @Autowired
+    private UserRepo repo;
+
+    @ResponseBody
+    @GetMapping(value = "/test")
+    public Optional<User> display(){
+        return repo.findByUserNameOrEmail("jobayerccc","");
+    }
+}
---
+++ cms/src/main/java/com/cms/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.cms.controller;
+
+
+import com.cms.entity.Role;
+import com.cms.entity.User;
+import com.cms.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserRepo repo;
+
+@Autowired
+private PasswordEncoder passwordEncoder;
+
+    @GetMapping(value = "/user-save")
+    public String saveUser() {
+        Set<Role> roles = new HashSet<>();
+        roles.add(new Role(1L));
+        roles.add(new Role(3L));
+        User user = new User("Mr Suvo", "suvo", passwordEncoder.encode("1234"), "suvo@gmail.com", true, roles);
+        repo.save(user);
+
+        Set<Role> roles2 = new HashSet<>();
+        roles2.add(new Role(2L));
+        User user2 = new User("Mr Rony", "rony", passwordEncoder.encode("1234"), "rony@gmail.com", true, roles2);
+        repo.save(user2);
+
+        Set<Role> roles3 = new HashSet<>();
+        roles3.add(new Role(3L));
+        User user3 = new User("Mr Motin", "motin", passwordEncoder.encode("1234"), "motin@gmail.com", true, roles3);
+        repo.save(user3);
+
+       Set<Role> roles4 = new HashSet<>();
+        roles4.add(new Role(1L));
+        roles4.add(new Role(2L));
+        roles4.add(new Role(3L));
+        User user4 = new User("MR Yasin", "yasin", passwordEncoder.encode("1234"), "yasin@gmail.com", true, roles4);
+        repo.save(user4);
+      return "success";
+    }
+}
