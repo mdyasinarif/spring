46f951a9b34bdd2dff2ec516b225a3ab15499322
mdyasinarif <mdyasinarifidb49@gmail.com>
2019-08-06 18:57:44 +0600

    security done databaseop list lool

--- residentInfo/src/main/java/com/resident/repo/BuillidingRepo.java
+++ residentInfo/src/main/java/com/resident/repo/BuillidingRepo.java
@@ -3,11 +3,15 @@ package com.resident.repo;
 
 
 import com.resident.entity.buliding.Builliding;
+import com.resident.entity.user.HouseOwner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BuillidingRepo extends JpaRepository<Builliding,Long> {
         Builliding findByName(String name);
         Builliding findByAdress(String address);
+         List<Builliding> findAllByHouseOwner(HouseOwner houseOwner);
 }
